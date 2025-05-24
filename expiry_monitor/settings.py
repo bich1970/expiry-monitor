f52ef3b24034001af73c5665700edc0b460eac05
bich1970 <bich1970@gmail.com>
2025-05-24 14:08:39 +0300

    Add back AWS S3 and decouple config to settings.py

--- expiry_monitor/settings.py
+++ expiry_monitor/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fb&mtodh5p^^hr9j@u6b-z52q31-$buxfu340lhfbi=y^yc$%!'
+SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
